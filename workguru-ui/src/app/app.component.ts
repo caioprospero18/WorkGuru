92a32a54dc916fa5e0a0f9f15e2bd6bf48a3b647
caioprospero18 <caioprospero18@gmail.com>
2025-01-16 22:13:07 -0300

    Ajustes na tela de registro de empresa

--- workguru-ui/src/app/app.component.ts
+++ workguru-ui/src/app/app.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router'; // Importação do Router e NavigationEnd
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'workguru-ui';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const body = document.body;
+        body.classList.remove('pagina-home');
+
+        if (this.router.url === '/home') {
+          body.classList.add('pagina-home'); 
+        }
+      }
+    });
+  }
 }
 
--- workguru-ui/src/app/home/home/home.component.ts
+++ workguru-ui/src/app/home/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { JobService } from '../../jobs/job.service';
 import { AuthService } from '../../security/auth.service';
